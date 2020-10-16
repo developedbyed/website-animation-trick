290d09d522683cd020afd7a8d68a5c7cbeb9512e
Raeeddoesgaming <69373544+Raeeddoesgaming@users.noreply.github.com>
2020-10-16 17:28:46 -0400

    Update app.js

--- app.js
+++ app.js
@@ -5,3 +5,4 @@ tl.to(".slider", { y: "-100%", duration: 1.5, delay: 0.5 });
 tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
 tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 1 });
 tl.fromTo(".big-text", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
+//test
